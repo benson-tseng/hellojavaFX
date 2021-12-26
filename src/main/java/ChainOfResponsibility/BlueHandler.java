0d305faeba459ce4c973a7a47fb775edd9f28e1c
yaotsung <er0106er@gmail.com>
2021-12-26 14:48:52 +0800

    update handler press ctrl + e or ctrl + b once text will be blue or bold, then press again will clean the fontstyle;

--- src/main/java/ChainOfResponsibility/BlueHandler.java
+++ src/main/java/ChainOfResponsibility/BlueHandler.java
@@ -1,23 +1,32 @@
 package ChainOfResponsibility;
 
 import Command.BlueCommand;
+import Command.CleanCommand;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 import java.security.Key;
 
 public class BlueHandler extends KeyHandler{
-
+    private int count = 0;
     public BlueHandler(KeyHandler kHandler) {super(kHandler);}
 
     //Ctrl E will do blue command
     @Override
     public void toHandle(KeyEvent event) {
         if (event.getCode() == KeyCode.E && event.isControlDown()){
-            BlueCommand blue = new BlueCommand(super.getFontStyle());
-            super.getCommandInvoker().execute(blue);
-            System.out.println("Blue");
+            count++;
+            if (count % 2 == 1) {
+                BlueCommand blue = new BlueCommand(super.getFontStyle());
+                super.getCommandInvoker().execute(blue);
+                System.out.println("Blue");
+            } else if (count % 2 == 0) {
+                CleanCommand clean = new CleanCommand(super.getFontStyle());
+                super.getCommandInvoker().execute(clean);
+                System.out.println("Clean");
+            }
         } else {
+            count = 0;
             toNext(event);
         }
     }
--- src/main/java/ChainOfResponsibility/BoldHandler.java
+++ src/main/java/ChainOfResponsibility/BoldHandler.java
@@ -1,21 +1,32 @@
 package ChainOfResponsibility;
 
+import Command.BlueCommand;
 import Command.BoldCommand;
+import Command.CleanCommand;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class BoldHandler extends KeyHandler{
-
+    private int count = 0;
     public BoldHandler (KeyHandler kHandler){super(kHandler);}
 
     //Ctrl B will do bold command
     @Override
     public void toHandle(KeyEvent event) {
         if (event.getCode() == KeyCode.B && event.isControlDown() ){
-            BoldCommand bold = new BoldCommand(super.getFontStyle());
-            super.getCommandInvoker().execute(bold);
-            System.out.println("Bold");
+            count++;
+            if (count % 2 == 1) {
+                BoldCommand bold = new BoldCommand(super.getFontStyle());
+                super.getCommandInvoker().execute(bold);
+                System.out.println("Bold");
+            } else if (count % 2 == 0) {
+                CleanCommand clean = new CleanCommand(super.getFontStyle());
+                super.getCommandInvoker().execute(clean);
+                System.out.println("Clean");
+            }
+
         } else {
+            count = 0;
             toNext(event);
         }
 
